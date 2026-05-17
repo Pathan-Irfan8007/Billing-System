0dedfdf5c1f364ee36524d1ad2fd5a2bd516eab5
Pathan-Irfan8007 <pathanirfan8007@gmail.com>
2026-05-17 11:01:13 +0530

    database integrated, get-api added, database fetched from backend

--- public/script.js
+++ public/script.js
@@ -7,3 +7,27 @@ const showPage = (pageID) => {
 
     document.getElementById(pageID).classList.add("active");
 }
+
+const getData = async () => {
+    try{
+        const res = await fetch('http://localhost:5000/customer')
+        const customerData = await res.json()
+
+        let output = ""
+        customerData.forEach(customer => {
+            output += `
+                <div id="customerData">
+                    <p><b>Name :</b> ${customer.name}</p><br>
+                    <p><b>Mobile No :</b> ${customer.mob}</p><br>
+                    <p><b>Product Name :</b> ${customer.product}</p><br>
+                    <p><b>Price :</b> ${customer.price}</p><br>
+                    <p><b>Payment Method :</b> ${customer.payment}</p><br>
+                </div>
+            `
+        })
+        document.getElementById('database').innerHTML = output
+    } catch (error){
+        console.log(`Something Went Wrong : ${error}`)
+    }
+}
+getData()
--- server.js
+++ server.js
@@ -1,11 +1,33 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const { MongoClient } = require('mongodb')
 const cors = require('cors')
 const path = require('path')
 
-app.use(cors())
+
+
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+const url = 'mongodb://localhost:27017'
+const client = new MongoClient(url)
+
+const dbName = "Billing-System"
+let db
+
+const connectDB = async () => {
+    try{
+        await client.connect()
+        db = client.db(dbName)
+        console.log("Database Connected Succefully.")
+    }catch(error){
+        console.log(`Something Went Wrong : ${error}`)
+    }
+}
+connectDB()
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
@@ -13,6 +35,12 @@ app.get('/', (req, res) => {
 
 app.get('/check', (req, res) => {
     res.send("This is Working...")
+})
+
+app.get('/customer', async (req, res) => {
+    const collection = db.collection('customer')
+    const customerData = await collection.find().toArray()
+    res.json(customerData)
 })
 
 app.listen(port, () => {
